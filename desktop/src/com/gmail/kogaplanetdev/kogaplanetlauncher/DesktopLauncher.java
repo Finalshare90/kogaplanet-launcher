8506609c09ddaed77ed6b0334ad026243afe38d8
Finalshare90 <isaacpmartins08@gmail.com>
2022-09-19 21:24:55 -0300

    nothing to worry

--- desktop/src/com/gmail/kogaplanetdev/kogaplanetlauncher/DesktopLauncher.java
+++ desktop/src/com/gmail/kogaplanetdev/kogaplanetlauncher/DesktopLauncher.java
@@ -16,6 +16,7 @@ public class DesktopLauncher {
 		
 		//config de gráficos e perfomance
 		config.setForegroundFPS(60);
+		config.setDecorated(false);
 		
 		//config relacionadas ao programa em si
 		config.setMaximized(true);
